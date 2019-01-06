538779643b43f42ee969f171e7bb0e0cbefb1ba4
Christopher Pitt <cgpitt@gmail.com>
2019-01-06 18:58:50 +0200

    Add code to render subscription images and switch tabs

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react"
 import {
   AsyncStorage,
   Button,
+  Image,
   ScrollView,
   Text,
   TextInput,
@@ -15,6 +16,7 @@ class App extends Component {
     podcasts: undefined,
     subscriptions: [],
     terms: undefined,
+    tab: "search",
   }
 
   async componentDidMount() {
@@ -22,41 +24,24 @@ class App extends Component {
       "subscriptions",
     )
 
+    const parsed = subscriptions
+      ? JSON.parse(subscriptions)
+      : []
+
     this.setState({
-      subscriptions: subscriptions
-        ? JSON.parse(subscriptions)
-        : [],
+      subscriptions: parsed,
+      tab: parsed.length > 0 ? "listen" : "search",
     })
   }
 
   render() {
-    return (
-      <View
-        style={{
-          width: "100%",
-          height: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: 25,
-        }}
-      >
-        <TextInput
-          style={{
-            width: "100%",
-            borderColor: "#e0e0e0",
-            borderWidth: 1,
-            borderRadius: 4,
-            padding: 10,
-          }}
-          onChange={this.onChangeTerms}
-        />
-        <Button
-          title="Search"
-          onPress={this.onPressSearch}
-        />
-        {this.renderPodcasts()}
-      </View>
-    )
+    const { tab } = this.state
+
+    if (tab === "search") {
+      return this.renderSearch()
+    }
+
+    return this.renderListen()
   }
 
   onChangeTerms = e => {
@@ -76,7 +61,116 @@ class App extends Component {
     })
   }
 
-  renderPodcasts = () => {
+  onPressSearchPodcast = async podcast => {
+    const { subscriptions: previous } = this.state
+
+    const subscriptions = [...previous, podcast]
+
+    this.setState({
+      subscriptions,
+    })
+
+    await AsyncStorage.setItem(
+      "subscriptions",
+      JSON.stringify(subscriptions),
+    )
+  }
+
+  onPressListenPodcast = podcast => {
+    alert(`Open ${podcast.collectionName}`)
+  }
+
+  renderSearch() {
+    return (
+      <View
+        style={{
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 25,
+        }}
+      >
+        {this.renderTabs()}
+        <TextInput
+          style={{
+            width: "100%",
+            borderColor: "#e0e0e0",
+            borderWidth: 1,
+            borderRadius: 4,
+            padding: 10,
+          }}
+          onChange={this.onChangeTerms}
+        />
+        <Button
+          title="Search"
+          onPress={this.onPressSearch}
+        />
+        {this.renderSearchPodcasts()}
+      </View>
+    )
+  }
+
+  renderTabs = () => {
+    const { tab } = this.state
+
+    return (
+      <View
+        style={{
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "center",
+          marginBottom: 10,
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => this.setState({ tab: "search" })}
+        >
+          <View
+            style={{
+              paddingTop: 10,
+              paddingBottom: 10,
+            }}
+          >
+            <Text
+              style={{
+                color:
+                  tab === "search" ? "#e0e0e0" : "#007afb",
+                fontSize: 18,
+                fontWeight: "bold",
+              }}
+            >
+              Search
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => this.setState({ tab: "listen" })}
+        >
+          <View
+            style={{
+              paddingTop: 10,
+              paddingBottom: 10,
+            }}
+          >
+            <Text
+              style={{
+                color:
+                  tab === "listen" ? "#e0e0e0" : "#007afb",
+                fontSize: 18,
+                fontWeight: "bold",
+              }}
+            >
+              Listen
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  renderSearchPodcasts = () => {
     const { podcasts, subscriptions } = this.state
 
     if (podcasts === undefined) {
@@ -106,7 +200,7 @@ class App extends Component {
         }}
       >
         {podcasts.map(podcast =>
-          this.renderPodcast(
+          this.renderSearchPodcast(
             podcast,
             subscriptionIds.includes(podcast.collectionId),
           ),
@@ -115,7 +209,7 @@ class App extends Component {
     )
   }
 
-  renderPodcast = (podcast, isSubscribed) => {
+  renderSearchPodcast = (podcast, isSubscribed) => {
     return (
       <TouchableOpacity
         key={podcast.collectionId}
@@ -124,7 +218,7 @@ class App extends Component {
             return
           }
 
-          this.onPressAvailablePodcast(podcast)
+          this.onPressSearchPodcast(podcast)
         }}
       >
         <View
@@ -146,18 +240,59 @@ class App extends Component {
     )
   }
 
-  onPressAvailablePodcast = async podcast => {
-    const { subscriptions: previous } = this.state
+  renderListen = () => {
+    const { subscriptions } = this.state
 
-    const subscriptions = [...previous, podcast]
+    return (
+      <View
+        style={{
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 25,
+        }}
+      >
+        {this.renderTabs()}
+        <ScrollView
+          style={{
+            flexGrow: 0,
+            width: "100%",
+            height: "50%",
+          }}
+        >
+          {subscriptions.map(podcast =>
+            this.renderListenPodcast(podcast),
+          )}
+        </ScrollView>
+      </View>
+    )
+  }
 
-    this.setState({
-      subscriptions,
-    })
-
-    await AsyncStorage.setItem(
-      "subscriptions",
-      JSON.stringify(subscriptions),
+  renderListenPodcast = podcast => {
+    return (
+      <TouchableOpacity
+        key={podcast.collectionId}
+        onPress={() => this.onPressListenPodcast(podcast)}
+      >
+        <View
+          style={{
+            width: "100%",
+            height: 200,
+          }}
+        >
+          <Image
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+            resizeMode="cover"
+            source={{
+              uri: podcast.artworkUrl600,
+            }}
+          />
+        </View>
+      </TouchableOpacity>
     )
   }
 }
